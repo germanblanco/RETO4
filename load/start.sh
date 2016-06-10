ae185ccaf6b55f2bf5f04e698806b7bdc848b746
German Blanco <german.blanco@bluetab.net>
2016-06-10 11:11:54 +0200

    Working environment variables.

---
+++ load/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+RATE=${RATE:-10}
+HITS=${HITS:-500}
+if [ -z ${THESERVER+x} ]; then 
+  docker run --link reto4server -e "RATE=$RATE" -e "HITS=$HITS" --name reto4load reto4load
+else
+  docker run -e "THESERVER=$THESERVER" -e "RATE=$RATE" -e "HITS=$HITS" --name reto4load reto4load
+fi
