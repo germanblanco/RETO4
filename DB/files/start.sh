56cfcb7e54fd687ae66c881f709591fc4c9f1f25
German Blanco <german.blanco@bluetab.net>
2016-06-11 11:25:12 +0200

    Starting with Cassandra scaling.

--- DB/files/start.sh
+++ DB/files/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-#cassandra -Rf &
+
+MYIP=`ip a | grep "global eth0" | awk '{print $2}' | sed "s/\/..//"`
+sed -i.bak "s/cluster_name: 'Test Cluster'/cluster_name: 'RETO4'/" /etc/cassandra/cassandra.yaml
+sed -i.bak "s/cluster_name: 'Test Cluster'/cluster_name: 'RETO4'/" /etc/cassandra/cassandra.yaml
+
+cat /etc/cassandra/cassandra.yaml
+
 /docker-entrypoint.sh cassandra -f &
 until cqlsh -e "describe keyspace system;"
 do
