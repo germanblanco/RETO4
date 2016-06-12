e7006ac995456262f0b0f61d17e4918327144a1a
German Blanco <german.blanco@bluetab.net>
2016-06-12 08:56:18 +0200

    Scaling cassandra, adding seeds.

--- DB/files/start.sh
+++ DB/files/start.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-MYIP=`ip a | grep "global eth0" | awk '{print $2}' | sed "s/\/..//"`
+until [[ $SEED1IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && 
+         $SEED2IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+do
+   SEED1IP=`curl http://rancher-metadata/2015-12-19/services/DB/containers/0/primary_ip`
+   SEED2IP=`curl http://rancher-metadata/2015-12-19/services/DB/containers/1/primary_ip`
+done
+
+MYIP=`curl http://rancher-metadata/2015-12-19/self/container/primary_ip`
+
 sed -i.bak "s/cluster_name: 'Test Cluster'/cluster_name: 'RETO4'/" /etc/cassandra/cassandra.yaml
-sed -i.bak "s/cluster_name: 'Test Cluster'/cluster_name: 'RETO4'/" /etc/cassandra/cassandra.yaml
+sed -i.bak "s/seeds: .*/seeds: \"$SEED1IP,$SEED2IP\"/" /etc/cassandra/cassandra.yaml
+sed -i.bak "s/listen_address: .*/listen_address: \"$MYIP\"/" /etc/cassandra/cassandra.yaml
+sed -i.bak "s/broadcast_address: .*/broadcast_address: \"$MYIP\"/" /etc/cassandra/cassandra.yaml
+sed -i.bak "s/broadcast_rpc_address: .*/broadcast_rpc_address: \"$MYIP\"/" /etc/cassandra/cassandra.yaml
 
 cat /etc/cassandra/cassandra.yaml
 
